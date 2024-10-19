e266539063dbeadadcf7aa08242ee57e016cae43
MiAkshay <108668453+MiAkshay@users.noreply.github.com>
2024-10-19 14:50:58 +0530

    Add files via upload

---
+++ DS Tools & Ecosystem.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Data Science Tools and Ecosystem
+
+# In this notebook, Data Science and Ecosystem are summarized
+
+# Languages list for data science->
+# 1.Python.
+# 2.SQL.
+# 3.C++.
+# 4.JAVA.
+# 5.R.
+
+#   Libraries used in Data Science->
+#   ggplot2.
+#   Numpy.  
+#   Scipy.
+#   Tensorflow.
+#   Pandas.
+#   Matplotlib.
+#   Keras.
+#   Scikit-learn.
+#   
+#   
+
+# |Data Science Tools|
+# |---|
+# |SAS|
+# |Apache Hadoop|
+# |Tensor Flow|
+
+# # Given examples of Arithmetic exaples
+
+# In[4]:
+
+
+(8*4)+6 # arithmetic expression
+
+
+# In[5]:
+
+
+300/60 # converting 300 mins to hours by divifding by 60
+
+
+#    _Objective_
+# * Languges of Data Science
+# * Libraries of Data Science
+# * Data Science tools
+# * How to provide cooments in cell
+
+# ## Author
+# 
+#    Akshay
